f29df4f9800cae41d042aaf7a93729995d790351
nikhil <nikhilgurugubelli2007@gmail.com>
2021-02-16 20:11:30 +0530

    wrecking ball

--- sketch.js
+++ sketch.js
@@ -3,82 +3,101 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
-var engine, world;
-var box1, pig1,pig3;
-var backgroundImg,platform;
-var bird, slingshot;
+var engine,world;
+var ground;
+var box1;
+var box2;
+var box3;
+var box4;
+var box5;
+var box6;
+var box7;
+var box8;
+var box9;
+var box10;
+var box11;
+var box12;
+var box13;
+var box14;
+var box15;
+var box16;
+var box17;
+var box18;
+var box19;
+var box20;
+var ball;
+var rope;
 
 
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
-}
+
 
 function setup(){
-    var canvas = createCanvas(1200,400);
+    
+    var canvas = createCanvas(3000,800);
     engine = Engine.create();
     world = engine.world;
+    ground = new Ground(600,600,1200,20)
+    box1 = new Box(900,100)
+    box2= new Box(900,100)
+    box3 = new Box(900,100)
+    box4 = new Box(900,100)
+    box5 = new Box(900,100)
+    box6 = new Box(900,100)
+    box7 = new Box(800,100)
+    box8 = new Box(800,100)
+    box9 = new Box(800,100)
+    box10= new Box(800,100)
+    box11 = new Box(800,100)
+    box12 = new Box(800,100)
+    box13 = new Box(700,100)
+    box14 = new Box(700,100)
+    box15 = new Box(700,100)
+    box16 = new Box(700,100)
+    box17 = new Box(700,100)
+    box18 = new Box(700,100)
+    box19= new Box(700,100)
+    box20 = new Box(700,100)
+    ball = new Ball(200,200)
+    rope = new Rope(ball.body,{x:500,y:50})
 
-
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
-
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(200,50);
-
-    //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
-    background(backgroundImg);
+    background(180)
+    
     Engine.update(engine);
-    //strokeWeight(4);
+    ground.display();
     box1.display();
     box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-
     box3.display();
     box4.display();
-    pig3.display();
-    log3.display();
-
     box5.display();
-    log4.display();
-    log5.display();
+    box6.display();
+    box7.display();
+    box8.display();
+    box9.display();
+    box10.display();
+    box11.display();
+    box12.display();
+    box13.display();
+    box14.display();
+    box15.display();
+    box16.display();
+    box17.display();
+    box18.display();
+    box19.display();
+    box20.display();
+    ball.display();
+    rope.display();
 
-    bird.display();
-    platform.display();
-    //log6.display();
-    slingshot.display();    
+
+
+
+
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    Matter.Body.setPosition(ball.body,{x:mouseX,y:mouseY})
 }
 
 
-function mouseReleased(){
-    slingshot.fly();
-}
-
-function keyPressed(){
-    if(keyCode === 32){
-        slingshot.attach(bird.body);
-    }
-}
